c08a722efdcbb0f6cd985658fc709a7459754b42
Russ Smith <russ@bashme.org>
2013-11-14 12:38:17 -0800

    Linking whole directory.

--- recipes/link_ssh_things.rb
+++ recipes/link_ssh_things.rb
@@ -3,20 +3,8 @@ include_recipe "sprout-osx-apps::dropbox"
 ssh_dir = "/Users/#{node['current_user']}/.ssh"
 ssh_dbox_dir = "/Users/#{node['current_user']}/Dropbox/Hold/ssh_config"
 
-
-directory ssh_dir do
+link ssh_dir do
+  to ssh_dbox_dir
   owner node['current_user']
   mode 0700
-end
-
-%w(
-  config
-  id_rsa
-  id_rsa.pub
-  ).each do |ssh_file|
-  link "#{ssh_dir}/#{ssh_file}" do
-    to "#{ssh_dbox_dir}/#{ssh_file}"
-    owner node['current_user']
-    mode 0600
-  end
 end
