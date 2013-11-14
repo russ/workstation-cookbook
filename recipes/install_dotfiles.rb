3f63c0f30405ae84d5b74ef74367ae53eecca4bf
Russ Smith <russ@bashme.org>
2013-11-14 15:04:12 -0800

    Updates.

--- recipes/install_dotfiles.rb
+++ recipes/install_dotfiles.rb
@@ -1,18 +1,22 @@
-#execute "install_dotfiles" do
-#  command "curl -L https://raw.github.com/russ/dotfiles/master/install.sh | sh"
-#  creates "~/.files"
-#  action :run
-#end
+execute 'install_dotfiles' do
+  cwd "/Users/#{node['current_user']}"
+  command 'git clone https://github.com/russ/dotfiles.git'
+  creates '~/.dotfiles'
+  action :run
+  not_if { ::File.exists?("/Users/#{node['current_user']}") }
+end
 
-# git "/Users/#{node['current_user']}/.files" do
-#   repository "https://github.com/russ/dotfiles.git"
-#   revision "master"
-#   notifies :run, "execute[symlink_dotfiles]"
-#   user node['current_user']
-# end
-# 
-# execute "symlink_dotfiles" do
-#   command "/Users/#{node['current_user']}/.files/symlink_dotfiles.sh"
-#   user node['current_user']
-#   action :nothing
-# end
+git "/Users/#{node['current_user']}/.dotfiles" do
+  repository 'https://github.com/russ/dotfiles.git'
+  revision 'master'
+  notifies :run, 'execute[symlink_dotfiles]'
+  user node['current_user']
+end
+
+execute 'symlink_dotfiles' do
+  cwd "/Users/#{node['current_user']}/.dotfiles"
+  command 'rake install'
+  user node['current_user']
+  creates '~/.bash_profile'
+  action :run
+end
---
+++ recipes/remove_bash_profile.rb
@@ -0,0 +1,3 @@
+file "/Users/#{node['current_user']}/.bash_profile" do
+  action :delete
+end
